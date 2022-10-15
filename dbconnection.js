414ca64db87969d74704558692af80ebd684fbcf
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-15 10:20:28 -0600

    good as its gonna get

--- dbconnection.js
+++ dbconnection.js
@@ -1,8 +1,20 @@
-const connection = {
-  host: process.env.HOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-};
-module.exports = connection;
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
 const mySQL = require("mysql2");
 require("console.table");
-const employees = require("./controllers/employees");
 require("dotenv").config();
 const express = require("express");
 
@@ -76,4 +75,96 @@ function welcome() {
           updateEmployee();
       }
     });
+}
+// -------------------------------
+function viewAllEmployees() {
+  let sql = `SELECT * FROM employee`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.table(result, ["firstName", "lastName", "department"]);
+  });
+  welcome();
+}
+
+// --------------------------------
+function viewDepartment() {
+  var query = `SELECT d.id, d.name, r.salary AS budget
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  GROUP BY d.id, d.name`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const departmentChoices = res.map((data) => ({
+      value: data.id,
+      name: data.name,
+    }));
+
+    console.table(res);
+    // connection.query("SELECT * FROM departments", function (err, result, fields) {
+    //   if (err) throw err;
+    // });
+    // let departmentList = [];
+    // result.forEach((element) => {
+    //   departmentList.push({
+    //     name: element.name,
+    //     value: element.name,
+    //   });
+    // });
+  });
+}
+function viewRole(assignRole, updateRole) {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeId",
+        message: "assign an employee a role.",
+        choices: assignRole,
+      },
+      {
+        type: "list",
+        name: "roleId",
+        message: "update a role.",
+        choices: updateRole,
+      },
+    ])
+    .then(function (answer) {
+      var query = `UPDATE employee SET role_id = ? WHERE id = ?`;
+      connection.query(
+        query,
+        [answer.roleId, answer.employeeId],
+        function (err, res) {
+          if (err) throw err;
+
+          welcome();
+        }
+      );
+    });
+}
+function addRole() {
+  var query = `SELECT d.id, d.name, r.salary AS budget
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  GROUP BY d.id, d.name`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const departmentChoices = res.map(({ id, name }) => ({
+      value: id,
+      name: `${id} ${name}`,
+    }));
+
+    console.table(res);
+
+    promptAddRole(departmentChoices);
+  });
 }
